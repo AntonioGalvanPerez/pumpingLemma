9c18bd9d8593a424ebc29fa51ac498ea173dcfd3
Antonio Galvan Perez <50242490+AntonioGalvanPerez@users.noreply.github.com>
2019-07-08 13:01:23 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,10 +4,69 @@
 //
 
 #include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+
 using namespace std;
+vector<char> parseVars(string inputString);
+vector<char> parseExpo(string inputString2);
+
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    cout << "Hello, World!\n";
+    string inputString;
+    vector <char> var;
+    int i = 0;
+    
+//Takes in user input
+    cout << "Enter up to three base variables seperated by commas.\n";
+    cin >> inputString;
+    var = parseVars(inputString);
+    
+    for(i = 0; i < var.size(); ++i){
+        cout << var[i];
+        cout << "\n";
+    }
+    
+    
+    cout << "Enter up to three exponents seperated by commas.\n";
+    cin >> inputString;
+    var = parseVars(inputString);
+    
+    for(i = 0; i < var.size(); ++i){
+        cout << var[i];
+        cout << "\n";
+    }
+
+    
+    
     return 0;
+}
+
+
+
+
+
+
+
+
+
+//Function that parses the input based on commas returns a vector of variables
+vector<char> parseVars(string inputString){
+    char temp;
+    char ch;
+    
+    stringstream ss;
+    vector <char> var;
+    // parses input
+    ss.str (inputString);
+    
+    while (!ss.eof()){
+        ss >> temp;
+        var.push_back(temp);
+        ss >> ch;
+    }
+
+    return var;
 }
